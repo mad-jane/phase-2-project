103437580e54d46b99670417a70efcc3c63cde4d
madeline <madeline.j.dick@gmail.com>
2023-03-26 17:59:26 -0500

    added song search function to search bar

--- music-project/src/App.js
+++ music-project/src/App.js
@@ -12,6 +12,8 @@ function App() {
 
     const [searchKey, setSearchKey] = useState("")
     const [artists, setArtists] = useState([])
+    const [songs, setSongs] = useState([])
+    // const [categories, setCategories] = useState([])
 
     useEffect(() => {
         const hash = window.location.hash
@@ -33,22 +35,22 @@ function App() {
         window.localStorage.removeItem("token")
     }
 
-    const searchArtists = async (e) => {
-      e.preventDefault()
-      const {data} = await axios.get("https://api.spotify.com/v1/search", {
-          headers: {
-              Authorization: `Bearer ${token}`
-          },
-          params: {
-              q: searchKey,
-              type: "artist"
-          }
-      })
-  
-      setArtists(data.artists.items)
-  }
+    const searchArtists = async () => {
+        // e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchKey,
+                type: "artist"
+            }
+        })
+    
+        setArtists(data.artists.items)
+    }
 
-  const renderArtists = () => {
+    const renderArtists = () => {
     return artists.map(artist => (
         <div key={artist.id}>
             {artist.images.length ? <img width={"30%"} src={artist.images[0].url} alt=""/> : <div>**No Image**</div>}
@@ -58,6 +60,58 @@ function App() {
     ))
 }
 
+const searchSongs = async () => {
+    // e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/tracks", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        params: {
+            q: searchKey,
+            type: "track"
+        }
+    })
+
+    setSongs(data.songs.items)
+}
+
+const renderSongs = () => {
+return songs.map(song => (
+    <div key={song.id}>
+        {song.images.length ? <img width={"30%"} src={song.images[0].url} alt=""/> : <div>**No Image**</div>}
+        <br/>
+        {song.name}
+    </div>
+))
+}
+
+// const searchGenres = async (e) => {
+//     e.preventDefault()
+//     const {data} = await axios.get("https://api.spotify.com/v1/browse/categories", {
+//         headers: {
+//             Authorization: `Bearer ${token}`
+//         },
+//         params: {
+//             q: searchKey,
+//             type: "category"
+//         }
+//     })
+
+//     setCategories(data.categories.items)
+// }
+
+// const renderGenres = () => {
+// return categories.map(category => (
+//     <div key={category.id}>
+//         {category.name}
+//     </div>
+// ))
+// }
+
+const handleSubmit = () => {
+    searchArtists()
+    searchSongs()
+}
 
     return (
         <div className="App">
@@ -67,10 +121,20 @@ function App() {
                     <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
                         to Spotify</a>
                     : <button onClick={logout}>Logout</button>}
-                    <form onSubmit={searchArtists}>
-                      <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-                      <button type={"submit"}>Search</button>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+                        <button type={"submit"}>Search</button>
                     </form>
+                    {/* <form onSubmit={searchSongs}>
+                        <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+                        <button type={"submit"}>Search</button>
+                    </form>
+                    <form onSubmit={searchGenres}>
+                        <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+                        <button type={"submit"}>Search</button>
+                    </form> */}
+                    {/* {renderGenres()} */}
+                    {renderSongs()}
                     {renderArtists()}
             </header>
         </div>
